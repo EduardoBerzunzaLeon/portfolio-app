60624b1ef6116d0db412c55299c823ada93b4fef
Eduardo Berzunza <eduardoberzunzal@gmail.com>
2024-02-26 14:54:53 -0600

    Grid layout: done

--- app/_components/about/About.tsx
+++ app/_components/about/About.tsx
@@ -2,16 +2,18 @@ import './about.scss';
 
 export const About = () => (
   <section className="about">
-    <h2>Nosotros</h2>
+    <div className="row">
+      <div className="about__content col-6">
+        <h2>Sobre mi</h2>
+        <div className="about__content-text">
+          {/* eslint-disable-next-line max-len */}
+          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quod, alias reprehenderit, sint rerum eos magni quas, quidem nemo fuga autem assumenda aliquam dicta tenetur voluptate nulla beatae blanditiis debitis excepturi?</p>
+        </div>
+      </div>
+      <div className="about__image col-6">
+        <img src="./profile.jpg" alt="profile" />
+      </div>
 
-    <div className="about__content">
-      <div className="about__content-text">
-        {/* eslint-disable-next-line max-len */}
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quod, alias reprehenderit, sint rerum eos magni quas, quidem nemo fuga autem assumenda aliquam dicta tenetur voluptate nulla beatae blanditiis debitis excepturi?</p>
-      </div>
-      <div className="about__content-image">
-        <h3>IMAGE HERE</h3>
-      </div>
     </div>
 
   </section>
--- app/_components/navbar/Navbar.tsx
+++ app/_components/navbar/Navbar.tsx
@@ -2,15 +2,19 @@ import './navbar.scss';
 
 export const Navbar = () => (
   <nav className="nav">
-    <span className="nav__heading">Eduardo Berzunza</span>
-    <div className="nav__links-box">
-      <a href="#home">Inicio</a>
-      <a href="#about">Nosotros</a>
-      <a href="#project">Proyectos</a>
-      <a href="#comments">Testimonios</a>
-      <a href="#contact">Contacto</a>
+    <div className="row row--center">
+      <span className="nav__heading col-3">Eduardo Berzunza</span>
+      <div className="nav__links-box col-7 w-full">
+        <a href="#home">Inicio</a>
+        <a href="#about">Nosotros</a>
+        <a href="#project">Proyectos</a>
+        <a href="#comments">Testimonios</a>
+        <a href="#contact">Contacto</a>
+      </div>
+      <div className="nav__settings col-2 w-full">
+        <a href="#blog">Blog</a>
+      </div>
     </div>
-    <a href="#blog">Blog</a>
   </nav>
 );
 
